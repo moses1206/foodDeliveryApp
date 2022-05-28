e312aaea83a2cd0ebe9767114c35c1eeb603b996
unknown <mose23@naver.comm>
2022-05-28 14:19:41 +0900

    2

--- AppInner.tsx
+++ AppInner.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import Settings from './src/pages/Settings';
 import Orders from './src/pages/Orders';
@@ -11,6 +11,7 @@ import {RootState} from './src/store/reducer';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import useSocket from './src/hooks/userSocket';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -31,6 +32,35 @@ function AppInner() {
   // useSelector는 Provider 내부에서만 사용할 수 있어서 AppInner를 만들고
   // App.tsx에서 Provider 안에 AppInner를 넣어준다.
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+
+  const [socket, disconnect] = useSocket();
+
+  useEffect(() => {
+    const helloCallback = (data: any) => {
+      console.log(data);
+    };
+    if (socket && isLoggedIn) {
+      console.log(socket);
+      // 서버에 보내기
+      // socket.emit('login', 'hello');
+      // 서버에서 받기
+      socket.on('hello', helloCallback);
+    }
+    return () => {
+      if (socket) {
+        // 서버 접속 종료하기
+        socket.off('hello', helloCallback);
+      }
+    };
+  }, [isLoggedIn, socket]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      console.log('!isLoggedIn', !isLoggedIn);
+      disconnect();
+    }
+  }, [isLoggedIn, disconnect]);
+
   return isLoggedIn ? (
     <Tab.Navigator>
       <Tab.Screen
